8066969adbf495377ec18a216547264fe370ed5e
juuulliieee <slee17@cca.edu>
2017-04-03 17:05:15 -0700

    dfd

---
+++ instagram filter/js/script.js
@@ -0,0 +1,244 @@
+$('document').ready(function(){
+	
+	/* find the element and change body's class on hover */
+	$('#filter-1').hover(function(){
+		$('body').addClass('amaro');
+	}, function(){
+		$('body').removeClass('amaro');
+	});
+
+	/*
+	$('#filter-1').click(function(){
+		alert('hi');
+	});
+	*/
+
+	$('#filter-2').hover(function(){
+		$('body').addClass('aden');
+	}, function(){
+		$('body').removeClass('aden');
+	});
+
+	$('#filter-3').hover(function(){
+		$('body').addClass('ashby');
+	}, function(){
+		$('body').removeClass('ashby');
+	});
+
+	$('#filter-4').hover(function(){
+		$('body').addClass('brannan');
+	}, function(){
+		$('body').removeClass('brannan');
+	});
+
+	$('#filter-5').hover(function(){
+		$('body').addClass('brooklyn');
+	}, function(){
+		$('body').removeClass('brooklyn');
+	});
+
+	$('#filter-6').hover(function(){
+		$('body').addClass('charmes');
+	}, function(){
+		$('body').removeClass('charmes');
+	});
+
+	$('#filter-7').hover(function(){
+		$('body').addClass('clarendon');
+	}, function(){
+		$('body').removeClass('clarendon');
+	});
+
+	$('#filter-8').hover(function(){
+		$('body').addClass('crema');
+	}, function(){
+		$('body').removeClass('crema');
+	});
+
+	$('#filter-9').hover(function(){
+		$('body').addClass('dogpatch');
+	}, function(){
+		$('body').removeClass('dogpatch');
+	});
+
+	$('#filter-10').hover(function(){
+		$('body').addClass('earlybird');
+	}, function(){
+		$('body').removeClass('earlybird');
+	});
+
+	$('#filter-11').hover(function(){
+		$('body').addClass('gingham');
+	}, function(){
+		$('body').removeClass('gingham');
+	});
+
+	$('#filter-12').hover(function(){
+		$('body').addClass('ginza');
+	}, function(){
+		$('body').removeClass('ginza');
+	});
+
+	$('#filter-13').hover(function(){
+		$('body').addClass('helena');
+	}, function(){
+		$('body').removeClass('helena');
+	});
+
+	$('#filter-14').hover(function(){
+		$('body').addClass('hudson');
+	}, function(){
+		$('body').removeClass('hudson');
+	});
+
+	$('#filter-15').hover(function(){
+		$('body').addClass('juno');
+	}, function(){
+		$('body').removeClass('juno');
+	});
+
+	$('#filter-16').hover(function(){
+		$('body').addClass('kelvin');
+	}, function(){
+		$('body').removeClass('kelvin');
+	});
+
+	$('#filter-17').hover(function(){
+		$('body').addClass('lark');
+	}, function(){
+		$('body').removeClass('lark');
+	});
+
+	$('#filter-18').hover(function(){
+		$('body').addClass('lo-fi');
+	}, function(){
+		$('body').removeClass('lo-fi');
+	});
+
+	$('#filter-19').hover(function(){
+		$('body').addClass('ludwig');
+	}, function(){
+		$('body').removeClass('ludwig');
+	});
+
+	$('#filter-20').hover(function(){
+		$('body').addClass('maven');
+	}, function(){
+		$('body').removeClass('maven');
+	});
+
+	$('#filter-21').hover(function(){
+		$('body').addClass('mayfair');
+	}, function(){
+		$('body').removeClass('mayfair');
+	});
+
+	$('#filter-22').hover(function(){
+		$('body').addClass('moon');
+	}, function(){
+		$('body').removeClass('moon');
+	});
+
+	$('#filter-23').hover(function(){
+		$('body').addClass('nashville');
+	}, function(){
+		$('body').removeClass('nashville');
+	});
+
+	$('#filter-24').hover(function(){
+		$('body').addClass('perpetua');
+	}, function(){
+		$('body').removeClass('perpetua');
+	});
+
+	$('#filter-25').hover(function(){
+		$('body').addClass('reyes');
+	}, function(){
+		$('body').removeClass('reyes');
+	});
+
+	$('#filter-26').hover(function(){
+		$('body').addClass('rise');
+	}, function(){
+		$('body').removeClass('rise');
+	});
+
+	$('#filter-27').hover(function(){
+		$('body').addClass('Sierra');
+	}, function(){
+		$('body').removeClass('Sierra');
+	});
+
+	$('#filter-28').hover(function(){
+		$('body').addClass('skyline');
+	}, function(){
+		$('body').removeClass('skyline');
+	});
+
+	$('#filter-29').hover(function(){
+		$('body').addClass('slumber');
+	}, function(){
+		$('body').removeClass('slumber');
+	});
+
+	$('#filter-30').hover(function(){
+		$('body').addClass('stinson');
+	}, function(){
+		$('body').removeClass('stinson');
+	});
+
+	$('#filter-31').hover(function(){
+		$('body').addClass('sutro');
+	}, function(){
+		$('body').removeClass('sutro');
+	});
+
+	$('#filter-32').hover(function(){
+		$('body').addClass('toaster');
+	}, function(){
+		$('body').removeClass('toaster');
+	});
+
+	$('#filter-33').hover(function(){
+		$('body').addClass('valencia');
+	}, function(){
+		$('body').removeClass('valencia');
+	});
+
+	$('#filter-34').hover(function(){
+		$('body').addClass('vesper');
+	}, function(){
+		$('body').removeClass('vesper');
+	});
+
+	$('#filter-35').hover(function(){
+		$('body').addClass('walden');
+	}, function(){
+		$('body').removeClass('walden');
+	});
+
+	$('#filter-36').hover(function(){
+		$('body').addClass('willow');
+	}, function(){
+		$('body').removeClass('willow');
+	});
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
